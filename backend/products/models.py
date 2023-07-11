0bf05a49ddaecc892a24c2a8b0f8d74af7e46c7d
Vijay Anand <vijayanand2k20@gmail.com>
2023-07-11 11:24:29 +0530

    Created a model and returned a data of an random instance of the model as JSON

---
+++ backend/products/models.py
@@ -0,0 +1,7 @@
+from decimal import Decimal
+from django.db import models
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    content = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=15, decimal_places=2, default=Decimal('99.99'))
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,8 +6,9 @@ import requests
 # endpoint = "https://httpbin.org/anything"
 endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint)
+# get_response = requests.get(endpoint, json={"query": "Hello World"}, params={"abc":123})
+get_response = requests.get(endpoint, json={"query": "Hello World"})
+# get_response = requests.get(endpoint)
 # print(get_response.text)
-print(get_response.status_code)
-print(get_response.json()['message'])
-# get_response = requestxs.get(endpoint, json={"query": "Hello World"})
+# print(get_response.status_code)
+print(get_response.json())
