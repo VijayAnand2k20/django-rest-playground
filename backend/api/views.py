ea29728e5e8b24ec22fffb38a927fd7211b2342f
Vijay Anand <vijayanand2k20@gmail.com>
2023-07-12 17:52:43 +0530

    Converted existing into basic rest framework method

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,18 @@ from django.http import JsonResponse, HttpResponse
 from django.forms.models import model_to_dict
 import json
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from products.models import Product
 
+@api_view(['GET','POST'])
 def api_home(request):
+    '''
+    DRF API View
+    '''
     model_data = Product.objects.all().order_by("?").first()
     data = {}
     if model_data:
         data = model_to_dict(model_data, fields=["id","title"])
-        # json_data_string = json.dumps(data)
-    # return HttpResponse(json_data_string, headers={"Content-Type": "application/json"})
-    return JsonResponse(data)
+    return Response(data)
