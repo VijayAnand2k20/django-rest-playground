4150c2773ca6a14a0fc82106116ad38758d604e7
Vijay <vijayanand2k20@gmail.com>
2023-07-17 13:55:38 +0530

    Understood few under the hood things

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListCreateAPIView.as_view(), name='create'),
+    path('', views.ProductMixinView.as_view(), name='create'),
+    # path('', views.ProductListCreateAPIView.as_view(), name='create'),
     path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='update'),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view(), name='delete'),
-    path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail'),
+    path('<int:pk>/', views.ProductMixinView.as_view(), name='detail'),
 ]
