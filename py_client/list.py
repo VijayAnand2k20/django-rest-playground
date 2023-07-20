a5aa452acb57ce9c1df9bc71c765942e47950ab2
Vijay <vijayanand2k20@gmail.com>
2023-07-20 12:11:00 +0530

    Basics of token based auth

--- py_client/list.py
+++ py_client/list.py
@@ -1,7 +1,23 @@
 import requests
+from getpass import getpass
 
-endpoint = "http://localhost:8000/api/products/"
+auth_endpoint = "http://localhost:8000/api/auth/"
+password = getpass()
 
-get_response = requests.get(endpoint)
+auth_response = requests.post(auth_endpoint, json={
+    "username": "staff",
+    "password": password,
+})
 
-print(get_response.json()) 
+print(auth_response.json())
+
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}",
+    }
+    endpoint = "http://localhost:8000/api/products/"
+
+    get_response = requests.get(endpoint, headers=headers)
+
+    print(get_response.json()) 
