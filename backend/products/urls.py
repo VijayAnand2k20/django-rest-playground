6ffc50ba71c6e2525ecab2b7161706fe2d2338fa
Vijay <vijayanand2k20@gmail.com>
2023-08-09 19:26:34 +0530

    Dynamic links generation in serializers

--- backend/products/urls.py
+++ backend/products/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 
+app_name = 'products-api'
+
 urlpatterns = [
     # path('', views.ProductMixinView.as_view(), name='create'),
     path('', views.ProductListCreateAPIView.as_view(), name='create'),
-    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='update'),
+    path('<int:pk>/update/', views.ProductUpdateAPIView.as_view(), name='edit'),
     path('<int:pk>/delete/', views.ProductDeleteAPIView.as_view(), name='delete'),
     path('<int:pk>/', views.ProductDetailAPIView.as_view(), name='detail'),
 ]
