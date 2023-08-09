907e1661bbb55656e0d3dd10ad0bca64609e91bd
Vijay <vijayanand2k20@gmail.com>
2023-08-09 17:59:47 +0530

    Revert "Learned about viewsets and routers"
    This reverts commit 5b0fe09b57bc611d001fe0bdaf8ee17566c00266.

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -7,7 +7,6 @@ class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
         fields = [
-            'pk',
             'title',
             'content',
             'price',
